cf8ba0efd2e54d4951fa2887729b8b137692a49e
BankierJulien <julienbankiernyc@gmail.com>
2017-05-04 17:04:46 -0400

    attempt one at fib not worked

--- ProjEuluer/ViewController.swift
+++ ProjEuluer/ViewController.swift
@@ -9,9 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var arrayOfEvenFibNum = [Int]()
+    var solution = Int()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.createFibSequenceFrom(a: 0, b: 1)
+        print(self.arrayOfEvenFibNum)
+        for num in self.arrayOfEvenFibNum{
+            print(solution)
+            solution = solution + num
+        }
+        print(solution)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +32,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+
+    func createFibSequenceFrom(a: Int,b: Int) -> Void{
+        if a < 400000{
+          //  print(fibNum)
+            let fibNum = a + b
+            if fibNum % 2 == 0 {
+                arrayOfEvenFibNum.append(fibNum)
+            }
+            self.createFibSequenceFrom(a: b, b: fibNum)
+        }
+    }
 
 }
 
