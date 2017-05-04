87ccb897f1dcc7757b6296070d04f2c5e784e7d5
BankierJulien <julienbankiernyc@gmail.com>
2017-05-04 16:31:05 -0400

    Initial Commit

---
+++ ProjEuluer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProjEuluer
+//
+//  Created by Julien Bankier on 5/4/17.
+//  Copyright © 2017 Julien Bankier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
