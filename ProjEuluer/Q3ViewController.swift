ed43b9888647b160b1eb9c5bf02a45ee9798756d
BankierJulien <julienbankiernyc@gmail.com>
2017-05-09 16:47:03 -0400

    sieve working, now need to finish

--- ProjEuluer/Q3ViewController.swift
+++ ProjEuluer/Q3ViewController.swift
@@ -17,7 +17,7 @@ class Q3ViewController: PEViewController {
         question.text = "The prime factors of 13195 are 5, 7, 13 and 29./n/n/n What is the largest prime factor of the number 600851475143 ?"
         
         //  self.generateListOfPrimes(ceiling: 10)
-        self.seiveEratosthenes(ceiling: 10)
+        self.facotrCheckForBoolArray(arrayOfBool:self.seiveEratosthenes(ceiling: 9) )
         
         
         
@@ -27,7 +27,7 @@ class Q3ViewController: PEViewController {
     
     
     //Sieve_of_Eratosthenes
-    func isItAFactor(numToFactor: Int, numToCheck: Int) -> Bool {
+    func factorCheck(numToFactor: Int, numToCheck: Int) -> Bool {
         if numToFactor % numToCheck == 0 {
             return true
         }
@@ -37,26 +37,36 @@ class Q3ViewController: PEViewController {
     }
     
     func seiveEratosthenes(ceiling:Int) -> Array<Bool>{
-        var boolArray = [Bool](repeating: true, count: ceiling)
+        // + 1 so array is not "zero based" for prime list, 0 included in list of number
+        var boolArray = [Bool](repeating: true, count: ceiling+1)
         let squareRootOfCeiling = Int(sqrt(Double(ceiling)))
-        
-        for i in 2..<boolArray.count where boolArray[i] {
-            for j in 2..<ceiling{
-                
-                if i*j<ceiling {
-                    
-                    boolArray[i*j] = false
+
+        // 1 and 2 always prime
+        boolArray[1] = true
+        boolArray[2] = true
+
+        for i in 2...squareRootOfCeiling{
+            if boolArray[i]{
+                for j in 0...squareRootOfCeiling{
+                    if (i*i + j*i)<=ceiling{
+                        boolArray[i*i + j*i] = false
+                    }
                 }
-                
             }
         }
-        
-        print(boolArray)
+        // remove 0 index so end list corresponds to 0 index = 1
+        boolArray.remove(at: 0)
         return boolArray
     }
     
-    
-    
-    
+    func facotrCheckForBoolArray(arrayOfBool:Array<Bool>) -> Int{
+        var counterOfFalse = 0
+          for i in 0...arrayOfBool.count-1{
+         // print(arrayOfBool.a)
+            
+            
+        }
+        return 0
+    }
     
 }
